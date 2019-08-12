d0aa5d867dc24e598c38bdec707313b53cdfd2d7
77web <info@77-web.com>
2019-08-12 16:48:16 +0900

    added Book entity(minimum)

---
+++ src/Entity/Book.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Book
+ *
+ * @ORM\Table
+ * @ORM\Entity()
+ * @package App\Entity
+ */
+class Book
+{
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
